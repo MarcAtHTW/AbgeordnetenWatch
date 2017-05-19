230003631b9105d0be2f8851ec90e89cf1f9cc6f
s0547068 <s0547068@htw-berlin.de>
2017-05-19 15:00:10 +0200

    - code strukuriert ueber functions - testing nltk stopwords

--- Testing_Steve.py
+++ Testing_Steve.py
@@ -1,47 +1,55 @@
 import PyPDF2
+from nltk.corpus import stopwords
 '''
-Data extracting from Plenarprotokoll
+    Data extracting from Plenarprotokoll
 '''
 
-txtDir = "C:/PycharmProjects/AbgeordnetenWatch/pdftotxt/"
-
-pdf_file = open('C:\PycharmProjects\AbgeordnetenWatch\Plenarprotokoll_18_232.pdf', 'rb')
-read_pdf = PyPDF2.PdfFileReader(pdf_file)
-#pages = read_pdf.getPage(0)
-#page_content = pages.extractText()
-page_content = ''
-
-'''
-get content fuer alle Seiten im Protokoll
-'''
-for i in range(read_pdf.getNumPages()):
-    print(i)
-    pages = read_pdf.getPage(i)
-    page_content = page_content + pages.extractText()
-    page_content
-
+''' get content fuer alle Seiten im Protokoll '''
+def getContent():
+    pdf_file = open('C:\PycharmProjects\AbgeordnetenWatch\Plenarprotokoll_18_232.pdf', 'rb')
+    read_pdf = PyPDF2.PdfFileReader(pdf_file)
+    page_content = ''
+    for i in range(read_pdf.getNumPages()):
+        print(i)
+        pages = read_pdf.getPage(i)
+        page_content += pages.extractText()
+    return page_content
 '''
 - function fuer content-splitt und vorhalten in Liste
 - Dabei entfernen von "\n" und "-" aus Listenelemente
 - zusätzliche Speicherung in txt-file
 '''
 def listToTxt(page_content):
+    txtDir = "C:/PycharmProjects/AbgeordnetenWatch/pdftotxt/"
     textFilename = txtDir + "Protokoll.txt"
     textFile = open(textFilename, "w")  # make text file
     list = page_content.split(' ')
     print(list)
-    for i in range(0, len(list)):
+    cleanList = []
+    for i in range(len(list)):
         list_element = list[i]
-        print(list_element)
         list_element = list_element.replace("\n", "")
         list_element = list_element.replace("-", "")
-        print("item at index", i, ":", list_element)
-        textFile.write(list_element+"\n")
-    textFile.close
-    print(list)
+        cleanList.append(list_element) # liste ohne -, \n
+        #print("item at index", i, ":", list_element)
+        #textFile.write(list_element+"\n")
+    return cleanList
+
+''' stop_words aus liste entfernen'''
+def filteredSentence(wordlist):
+    #set stop words
+    stop_words = set(stopwords.words("german"))
+    print(stop_words)
+    filtered_sentence = [w for w in wordlist if not w in stop_words]
+    print(filtered_sentence)
+    return filtered_sentence
 
 
-listToTxt(page_content)
+content = getContent()
+wordlist = listToTxt(content)
+filteredSentence(wordlist)
+
+
 
 
 
