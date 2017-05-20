8fca763133909ab461224f6625726f6c061ff424
s0547068 <s0547068@htw-berlin.de>
2017-05-20 15:28:29 +0200

    - code neu strukturiert - Dictionary befuellen - Datenbank befuellen - Analyse Redetext – Setzen von Haufigkeit und lexikalische Diversitaet - Testing stopword und Visualisierung

--- Testing_Steve.py
+++ Testing_Steve.py
@@ -1,5 +1,8 @@
 import PyPDF2
+from nltk import FreqDist
 from nltk.corpus import stopwords
+import operator
+
 '''
     Data extracting from Plenarprotokoll
 '''
@@ -19,10 +22,10 @@ def getContent():
 - Dabei entfernen von "\n" und "-" aus Listenelemente
 - zusätzliche Speicherung in txt-file
 '''
-def listToTxt(page_content):
+def contentToList(page_content):
     txtDir = "C:/PycharmProjects/AbgeordnetenWatch/pdftotxt/"
     textFilename = txtDir + "Protokoll.txt"
-    textFile = open(textFilename, "w")  # make text file
+    #textFile = open(textFilename, "w")  # make text file
     list = page_content.split(' ')
     print(list)
     cleanList = []
@@ -35,16 +38,11 @@ def listToTxt(page_content):
         #textFile.write(list_element+"\n")
     return cleanList
 
-''' stop_words aus liste entfernen'''
-def filteredSentence(wordlist):
-    #set stop words
-    stop_words = set(stopwords.words("german"))
-    print(stop_words)
-    filtered_sentence = [w for w in wordlist if not w in stop_words]
-    print(filtered_sentence)
-    return filtered_sentence
 
 
+
+
+''' xxxxxxxxxxxxxxxxxxxxxxxxxxx   Dictionary befuellen   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx '''
 #start with an empty list
 dl_protocol = []
 
@@ -69,11 +67,76 @@ for datarows in dl_protocol:
 datarow['tag']
 
 
+''' xxxxxxxxxxxxxxxxxxxxxxxxxxx   Datenbank befuellen   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx '''
+
+
+
+
+
+
+
+
+
+''' xxxxxxxxxxxxxxxxxxxxxxxxx   Analyse Redetext - Haufigkeit und lexikalische Diversitaet   xxxxxxxxxxxxxxxxxxxxxxxxxxxx '''
+
+def clean_and_getFrequenz(wordlist):
+    # RedeText enthealt noch  „!“, „,“, „.“ und Doppelungen und so weiter
+    print("Anzahl aller Woerter und Zeichen: " + str(len(wordlist)))
+    # saebern des RedeTextes von Zeichen !!! ABER !!! doppelte Woerter lassen, da die Haeufigkeit spaeter gezaehlt werden soll
+    cleaned_speech = [word for word in wordlist if word.isalpha()]
+    print("Anzahl aller Woerter - AUCH DOPPELTE ohne Zeichen: " + str(len(cleaned_speech)))
+    return cleaned_speech
+
+def count_and_viz_seldom_frequently(freq_CleandedSpeech):
+    # 30 haeufigsten und seltensten Woerter einer gesaeuberten Rede
+    dc_sort = (sorted(freq_CleandedSpeech.items(), key= operator.itemgetter(1), reverse = True))   # sortiertes dictionary - beginnend mit groeßter Haeufigkeit
+    print(dc_sort[:30])                         # 30 haeufigsten Woerter (Wort: Anzahl)
+    print([str(w[0]) for w in dc_sort[:30]])    # 30 haeufigsten Woerter (nur Wort)
+    print(dc_sort[-30:])                        # 30 seltensten Woerter (Wort: Anzahl)
+    print([str(w[0]) for w in dc_sort[-30:]])   # 30 seltensten Woerter (nur Wort)
+
+    # Wir koennen uns auch eine kummulative Verteilungsfunktion grafisch anzeigen lassen. Dazu können wir die plot()-Methode
+    # auf dem fdist1-Objekt anwenden. Dazu muss jedoch das Modul matplotlib installiert sein!
+    freq_CleandedSpeech.plot(30, cumulative=True)
+
+def lex_div_with_and_without_stopwords(wordlist, cleaned_speech):
+    ###### Lexikalische Diversität eines Redners - Vielzahl von Ausdrucksmöglichkeiten #######
+    # Die Diversität ist ein Maß für die Sprachvielfalt. Sie ist definiert als Quotient der „verschiedenen Wörter“ dividiert durch die „Gesamtanzahl von Wörtern“ eines Textes.
+
+    # Redetext mit stop words
+    freq_CleandedSpeech_with_stopwords = FreqDist(cleaned_speech)           # methode FreqDist() - Ermittlung der Vorkommenshaeufigkeit der Woerter im gesaeuberten RedeText
+    print(freq_CleandedSpeech_with_stopwords)
+    count_and_viz_seldom_frequently(freq_CleandedSpeech_with_stopwords)     # Visualisieren der haufigsten und seltensten Woerter inclusive stopwords
+
+    complete_text_with_doubles = list(freq_CleandedSpeech_with_stopwords)        # noch doppelte Woerter enthalten
+    diff_words = set(complete_text_with_doubles)                                 # "diff_words" enthaelt keine doppelten Woerter mehr
+    diversity_with_stopwords = len(diff_words) / float(len(complete_text_with_doubles))
+
+    # Redetext ohne stop words
+    stop_words = set(stopwords.words("german"))
+    print("\n" + "STOPWORDS: " + "\n" + str(stop_words) + "\n")
+
+    clean_without_stopwords = [word for word in cleaned_speech if not word in stop_words]                       # herausfiltern der stopwords
+    freq_Cleanded_without_stopwords =  FreqDist(clean_without_stopwords)                                        # Neuzuweisung: methode FreqDist() - Ermittlung der Vorkommenshaeufigkeit der Woerter im gesaeuberten RedeText ohne stopwords
+    complete_text_with_doubles_without_stopwords = list(freq_Cleanded_without_stopwords)
+    diff_words_without_doubles = set(complete_text_with_doubles_without_stopwords)                              # "diff_words_without_doubles" enthaelt keine doppelten Woerter mehr
+    diversity_without_stopwords = len(diff_words_without_doubles) / float(len(complete_text_with_doubles_without_stopwords))
+
+    print(freq_Cleanded_without_stopwords)
+    count_and_viz_seldom_frequently(freq_Cleanded_without_stopwords)  # Visualisieren der haufigsten und seltensten Woerter ohne stopwords
+
+    name = "Redner: " + 'xxxxxxxxxxxxxxxxxxxx'
+    print(name)
+    print("different words:    {0:8d}".format(len(diff_words)))                                     # Anzahl unterschiedlich einmalig genutzter Woerter
+    print("words:              {0:8d}".format(len(wordlist)))                                       # Anzahl genutzter Woerter
+    print("lexical diversity with stopwords:  {0:8.2f}".format(diversity_with_stopwords))           # Prozentsatz fuer die Sprachvielfalt mit stopwords
+    print("lexical diversity without stopwords:  {0:8.2f}".format(diversity_without_stopwords))     # Prozentsatz fuer die Sprachvielfalt ohne stopwords
+
+
+
 content = getContent()
-wordlist = listToTxt(content)
-filteredSentence(wordlist)
-
-
-
+wordlist = contentToList(content)
+cleaned_Speech = clean_and_getFrequenz(wordlist)
+lex_div_with_and_without_stopwords(wordlist, cleaned_Speech)
 
 
