1f432c3bcd36bcab467966579c9e0536b35033a4
Marc Sahib <marc.sahib@init.de>
2017-05-30 13:28:23 +0200

    Einrichtung Marc

--- Testing_Steve.py
+++ Testing_Steve.py
@@ -232,16 +232,7 @@ def lex_div_with_and_without_stopwords(wordlist, cleaned_speech):
     print("lexical diversity with stopwords:  {0:8.2f}".format(diversity_with_stopwords))           # Prozentsatz fuer die Sprachvielfalt mit stopwords
     print("lexical diversity without stopwords:  {0:8.2f}".format(diversity_without_stopwords))     # Prozentsatz fuer die Sprachvielfalt ohne stopwords
 
-#test
-#test1234
-#test vini
-
-
-
 content = getContent()
 wordlist = contentToList(content)
 cleaned_Speech = clean_and_getFrequenz(wordlist)
 lex_div_with_and_without_stopwords(wordlist, cleaned_Speech)
-
-
-#test
