54dc8ee8507c8648801f915a47dbf9e64db8d34b
MarcAtHTW <s0547375@htw-berlin.de>
2017-06-02 02:01:43 +0200

    Teständerung zur Erstellung der GitHub-Anleitung in Trello

--- Testing_Marc.py
+++ Testing_Marc.py
@@ -19,20 +19,6 @@ from nltk import ne_chunk
     Data extracting from Plenarprotokoll
 '''
 
-class Logger(object):
-    def __init__(self):
-        self.terminal = sys.stdout
-        self.log = open("log/console-output/output_log.txt'", 'w')
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        #this flush method is needed for python 3 compatibility.
-        #this handles the flush command by doing nothing.
-        #you might want to specify some extra behavior here.
-        pass
 
 ''' get content fuer alle Seiten im Protokoll '''
 def getContent():
